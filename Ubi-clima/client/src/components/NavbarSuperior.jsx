523bfde05e77886357604a5033d684121fd84adb
Dreykolin <dreykop2000@gmail.com>
2025-05-02 17:36:26 -0400

    se añadió imágen y logo

--- Ubi-clima/client/src/components/NavbarSuperior.jsx
+++ Ubi-clima/client/src/components/NavbarSuperior.jsx
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom';
 import '../assets/NavBar.css';
+import sun from '../images/sun.png'; // Importas la imagen
 
 export default function NavbarSuperior({ isLogged, handleLoginRedirect, handleLogout }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid d-flex justify-content-between">
+      <div className="container-fluid d-flex justify-content-between align-items-center">
         
-        {/* Nombre de la app a la izquierda */}
-        <span className="navbar-brand mb-0 h1">WeatherAct</span>
+        {/* Logo + nombre de la app */}
+        <div className="d-flex align-items-center">
+          <img src={sun} alt="WeatherAct Logo" className="navbar-logo me-2" />
+          <span className="navbar-brand mb-0 h1">WeatherAct</span>
+        </div>
         
-        {/* Links de navegación a la izquierda del botón de Iniciar sesión */}
+        {/* Links de navegación */}
         <ul className="nav nav-pills">
           <li className="nav-item">
             <Link className="nav-link" to="/">Inicio</Link>
@@ -18,8 +22,8 @@ export default function NavbarSuperior({ isLogged, handleLoginRedirect, handleLo
             <Link className="nav-link" to="/activities">Actividades</Link>
           </li>
         </ul>
-        
-        {/* Botón de login o logout a la derecha */}
+
+        {/* Botón login/logout */}
         <div>
           {isLogged ? (
             <button className="btn btn-danger" onClick={handleLogout}>Cerrar sesión</button>
